6c9269e55bac09789fb6a1f6183d492c8c759697
siliosilvestre@infoway-pi.com.br <codes123>
2019-06-26 10:13:57 -0300

    organizando a lista de linhas corretamente

--- lib/screens/linhas_screen.dart
+++ lib/screens/linhas_screen.dart
@@ -17,6 +17,8 @@ class LinhasScreen extends StatefulWidget {
 class _LinhasScreenState extends State<LinhasScreen> {
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  LinhasBloc _linhasBloc;
+
   String _query;
 
   _callSearch() async {
@@ -30,7 +32,7 @@ class _LinhasScreenState extends State<LinhasScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final _linhasBloc = BlocProvider.of<LinhasBloc>(context);
+    _linhasBloc = BlocProvider.of<LinhasBloc>(context);
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
@@ -50,14 +52,15 @@ class _LinhasScreenState extends State<LinhasScreen> {
       ),
       body: StreamBuilder<List<DocumentSnapshot>>(
           stream: _linhasBloc.outLinhas,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
+          builder: (context, snapshotLinhas) {
+            if (!snapshotLinhas.hasData) {
               return Center(child: CircularProgressIndicator());
             } else {
               if (_query != null && _query.trim().isNotEmpty) {
                 // filtra localmente
-                var documents = snapshot.data.where((snapshot) => snapshot
-                    .data['CodigoLinha'].toLowerCase()
+                var documents = snapshotLinhas.data.where((snapshot) => snapshot
+                    .data['CodigoLinha']
+                    .toLowerCase()
                     .toString()
                     .contains(removeDiacritics(_query.toLowerCase())));
 
@@ -67,25 +70,83 @@ class _LinhasScreenState extends State<LinhasScreen> {
 
                 return _createListLinhas(documents);
               }
-              if (snapshot.data == null || snapshot.data.isEmpty) {
+              if (snapshotLinhas.data == null || snapshotLinhas.data.isEmpty) {
                 return PrimeiramenteCadastreParaVer();
               }
-              return _createListLinhas(snapshot.data);
+              return _createListLinhas(snapshotLinhas.data);
             }
           }),
     );
   }
 
   _createListLinhas(Iterable<DocumentSnapshot> documents) {
-    return ListView.separated(
-      itemCount: documents.length,
-      itemBuilder: (content, index) {
-        return LinhaTile(documents.elementAt(index));
-      },
-      separatorBuilder: (context, index) => Divider(
-            indent: 16,
-            height: 1,
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          StreamBuilder<List<String>>(
+            stream: _linhasBloc.outLinhasFavoritas,
+            builder: (context, snapshotFavoritas) {
+              if (!snapshotFavoritas.hasData ||
+                  snapshotFavoritas.data.isEmpty) {
+                return Container();
+              }
+              return Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text("Suas linhas favoritas"),
+                  ),
+                  Column(
+                    children: documents
+                        .where((linha) => snapshotFavoritas.data
+                            .contains(linha["CodigoLinha"]))
+                        .toList()
+                        .map((linha) {
+                      return LinhaTile(linha);
+                    }).toList(),
+                  )
+                ],
+              );
+            },
           ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              ListTile(
+                title: Text("Linhas de Teresina"),
+              ),
+            ]..add(Column(
+                children: documents.map((linha) {
+                  return LinhaTile(linha);
+                }).toList(),
+              )),
+          )
+        ],
+      ),
     );
+
+//    return SingleChildScrollView(
+//      child: Column(
+//        children: <Widget>[
+//          StreamBuilder<List<String>>(
+//            stream: _linhasBloc.outLinhasFavoritas,
+//
+//          )
+//        ],
+//      ),
+//    );
+
+//    return ListView(
+//      children: Stre,
+//    );
+//    return ListView.separated(
+//      itemCount: documents.length,
+//      itemBuilder: (content, index) {
+//        return LinhaTile(documents.elementAt(index));
+//      },
+//      separatorBuilder: (context, index) => Divider(
+//            indent: 16,
+//            height: 1,
+//          ),
+//    );
   }
 }
